f2bb691cb0004faa43141219b8009fd9412888ad
Parga Cacheiro, Dominic <dominic@parga.io>
2022-12-04 06:53:37 +0100

    Finish day 2 rock paper scissors

--- december2022/day02/day02.go
+++ december2022/day02/day02.go
@@ -4,19 +4,66 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"sync"
 )
 
-var encryptionTable = map[string]string{
-	"A": "Rock",
-	"B": "Paper",
-	"C": "Scissors",
-	"X": "Rock",
-	"Y": "Paper",
-	"Z": "Scissors",
+const rockVs = "A"
+const paperVs = "B"
+const scissorsVs = "C"
+
+// default strategy
+const defaultStrategyName = "default"
+const rockMe = "X"
+const paperMe = "Y"
+const scissorsMe = "Z"
+
+// ultra top secret strategy
+const ultraTopSecretStrategyName = "ultraTopSecret"
+const meLosing = "X"
+const meDrawing = "Y"
+const meWinning = "Z"
+
+const lossScore = 0
+const drawScore = 3
+const winScore = 6
+const rockMeScore = 1
+const paperMeScore = 2
+const scissorsMeScore = 3
+
+var scoreTable = map[string]map[string]int{
+	defaultStrategyName: {
+		rockVs + " " + rockMe:         rockMeScore + drawScore,
+		rockVs + " " + paperMe:        paperMeScore + winScore,
+		rockVs + " " + scissorsMe:     scissorsMeScore + lossScore,
+		paperVs + " " + rockMe:        rockMeScore + lossScore,
+		paperVs + " " + paperMe:       paperMeScore + drawScore,
+		paperVs + " " + scissorsMe:    scissorsMeScore + winScore,
+		scissorsVs + " " + rockMe:     rockMeScore + winScore,
+		scissorsVs + " " + paperMe:    paperMeScore + lossScore,
+		scissorsVs + " " + scissorsMe: scissorsMeScore + drawScore,
+	},
+	ultraTopSecretStrategyName: {
+		rockVs + " " + meLosing:      scissorsMeScore + lossScore,
+		rockVs + " " + meDrawing:     rockMeScore + drawScore,
+		rockVs + " " + meWinning:     paperMeScore + winScore,
+		paperVs + " " + meLosing:     rockMeScore + lossScore,
+		paperVs + " " + meDrawing:    paperMeScore + drawScore,
+		paperVs + " " + meWinning:    scissorsMeScore + winScore,
+		scissorsVs + " " + meLosing:  paperMeScore + lossScore,
+		scissorsVs + " " + meDrawing: scissorsMeScore + drawScore,
+		scissorsVs + " " + meWinning: rockMeScore + winScore,
+	},
+}
+
+func computeRoundScore(round string, strategy string, roundScoreChannel chan<- int) {
+	choiceList := strings.Fields(round)
+	roundScoreChannel <- scoreTable[strategy][choiceList[0]+" "+choiceList[1]]
 }
 
 type Config struct {
 	inputFilepath string
+	strategy      string
 }
 
 func parseCmdline(args []string) Config {
@@ -24,12 +71,20 @@ func parseCmdline(args []string) Config {
 	var helpMsg string
 
 	helpMsg = "The Elves' encrypted strategy guide for Rock Paper Scissors"
-	inputFilepath := flagSet.String("encryptedStrategyGuide.txt", "", helpMsg)
+	inputFilepath := flagSet.String("calorie-group-list-file", "", helpMsg)
+
+	helpMsg = "Default strategy or ultra top secret strategy"
+	isUsingUltraTopSecretStrategy := flagSet.Bool("ultra-top-secret-strategy", false, helpMsg)
 
 	flagSet.Parse(args)
 
+	strategy := defaultStrategyName
+	if *isUsingUltraTopSecretStrategy {
+		strategy = ultraTopSecretStrategyName
+	}
 	return Config{
 		inputFilepath: *inputFilepath,
+		strategy:      strategy,
 	}
 }
 
@@ -37,12 +92,27 @@ func Run(args []string) error {
 	config := parseCmdline(args)
 
 	var err error
-	content, err := os.ReadFile(config.inputFilepath)
+	strategyGuide, err := os.ReadFile(config.inputFilepath)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(content)
+	waitGroup := new(sync.WaitGroup)
+	roundScoreChannel := make(chan int)
+	roundList := strings.Split(string(strategyGuide), "\n")
+	for _, round := range roundList {
+		waitGroup.Add(1)
+		go computeRoundScore(round, config.strategy, roundScoreChannel)
+	}
+	totalScore := 0
+	go func() {
+		for {
+			totalScore += <-roundScoreChannel
+			waitGroup.Done()
+		}
+	}()
+	waitGroup.Wait()
+	fmt.Println("Total score:", totalScore)
 
 	return nil
 }
