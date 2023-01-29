c83b69a289f606528924bae830ad2bfb0fd94694
Parga Cacheiro, Dominic <dominic@parga.io>
2023-01-29 17:40:35 +0100

    wip

--- december2022/day07/datastructures.go
+++ december2022/day07/datastructures.go
@@ -11,11 +11,46 @@ type task struct {
 }
 
 type AbsFile struct {
-	name *fileName
 	path []fileName
-	size fileSize
+	size *fileSize
 }
 
-func (absFile AbsFile) IsDirectory() bool {
-	return absFile.name == nil
+type FileNode struct {
+	name     *fileName
+	children []FileNode
+	size     *fileSize
+}
+
+func (fileNode *FileNode) Init() *FileNode {
+	fileNode.name = nil
+	fileNode.children = []FileNode{}
+	fileNode.size = nil
+	return fileNode
+}
+
+func (fileNode *FileNode) Add(absFile AbsFile) error {
+	current := fileNode
+
+	for _, p := range absFile.path {
+		var newCurrent *FileNode
+		if current.name == nil {
+			*current.name = p
+			// } else if *current.name != p {
+			// 	return fmt.Errorf("non matching filenames %v and %v for path %v", current.name, p, absFile.path)
+		} else {
+			for _, child := range current.children {
+				if p == *child.name {
+					newCurrent = &child
+					break
+				}
+			}
+		}
+		if newCurrent == nil {
+			newCurrent = new(FileNode).Init()
+		}
+		current.children = append(current.children, *newCurrent)
+		current = newCurrent
+	}
+
+	return nil
 }
--- december2022/day07/day07.go
+++ december2022/day07/day07.go
@@ -22,55 +22,48 @@ func splittedNameAndRest(cdSplittedLine cdSplittedLine) (fileName, cdFreeLine) {
 	return tmp[0], tmp[1]
 }
 
-func builtTaskList(cdSplittedLineList []cdSplittedLine) *[]*task {
-	taskList := []*task{}
+func builtTaskList(cdSplittedLineList []cdSplittedLine) []task {
+	taskList := []task{}
 
 	path := []fileName{}
-	for _, cdSplittedLine := range cdSplittedLineList[0:] {
+	for _, cdSplittedLine := range cdSplittedLineList {
 		dirname, cdFreeLine := splittedNameAndRest(cdSplittedLine)
 		if dirname != ".." {
 			path = append(path, dirname)
 			// manual deepcopy since path is a slice, hence shallow copies otherwise
 			newPath := make([]fileName, len(path))
 			copy(newPath, path)
-			taskList = append(taskList, &task{path: newPath, cdFreeLine: cdFreeLine})
+			taskList = append(taskList, task{path: newPath, cdFreeLine: cdFreeLine})
 		} else {
 			path = path[:len(path)-1]
 		}
 	}
 
-	return &taskList
+	return taskList
 }
 
-func builtFileTree(taskList *[]*task) ([]*AbsFile, fileSize) {
+func builtFileTree(taskList []task) FileNode {
 	waitGroup := new(sync.WaitGroup)
-	fileChannel := make(chan *[]*AbsFile)
-	for _, t := range *taskList {
+	fileChannel := make(chan []AbsFile)
+	for _, t := range taskList {
 		waitGroup.Add(1)
-		task := *t
-		go convertTaskToAbsFileList(task, fileChannel)
+		go convertTaskToAbsFileList(t, fileChannel)
 	}
 
-	fileList := []*AbsFile{}
-	var totalSize fileSize = 0
+	fileNode := new(FileNode).Init()
 	go func() {
 		for {
-			for _, f := range *<-fileChannel {
-				fileList = append(fileList, f)
-				totalSize += f.size
-			}
-
+			receiveFileList(fileNode, fileChannel)
 			waitGroup.Done()
 		}
 	}()
 
 	waitGroup.Wait()
-	// wrong: 49192532
-	return fileList, totalSize
+	return *fileNode
 }
 
-func convertTaskToAbsFileList(t task, fileChannel chan<- *[]*AbsFile) {
-	fileList := []*AbsFile{}
+func convertTaskToAbsFileList(t task, fileChannel chan<- []AbsFile) {
+	fileList := []AbsFile{}
 
 	lineList := strings.Split(t.cdFreeLine, "\n")
 	for _, line := range lineList {
@@ -81,37 +74,40 @@ func convertTaskToAbsFileList(t task, fileChannel chan<- *[]*AbsFile) {
 			continue
 		}
 
-		name := new(fileName)
-		var path []fileName
-		var size fileSize
+		var name fileName
+		var size *fileSize
 		if len(line) >= 4 && line[0:4] == "dir " {
-			// is directory
-
-			name = nil
-
-			path = make([]fileName, len(t.path))
-			copy(path, t.path)
-			path = append(path, line[4:])
-
-			size = 0
+			name = line[4:]
+			size = nil
 		} else {
-			// is file
-
 			tmp := strings.Split(line, " ")
 
-			*name = tmp[1]
+			name = tmp[1]
 
-			size, _ = strconv.ParseUint(tmp[0], 10, 0)
+			val, _ := strconv.ParseUint(tmp[0], 10, 0)
+			*size = val
 		}
 
-		fileList = append(fileList, &AbsFile{
-			name: name,
+		path := make([]fileName, len(t.path))
+		copy(path, t.path)
+		path = append(path, name)
+		fileList = append(fileList, AbsFile{
 			path: path,
 			size: size,
 		})
 	}
 
-	fileChannel <- &fileList
+	fileChannel <- fileList
+}
+
+func receiveFileList(fileNode *FileNode, fileChannel <-chan []AbsFile) {
+	in := <-fileChannel
+	for _, f := range in {
+		if fileNode.name == nil {
+			fileNode.name = &f.path[0]
+		}
+		fileNode.Add(f)
+	}
 }
 
 func Run(args []string) error {
@@ -128,8 +124,9 @@ func Run(args []string) error {
 	}
 
 	taskList := builtTaskList(splittedByCDCommands(string(logByteList)))
-	_, size := builtFileTree(taskList)
-	fmt.Println("Directory size:", size)
+	fileNode := builtFileTree(taskList)
+	// wrong: 49192532
+	fmt.Println("Directory size:", fileNode.size)
 
 	return err
 }
